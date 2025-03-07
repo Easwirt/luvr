67b66201b46f7a9c3a49b745b9f9d7c3d06e7efb
Viktor <viktor.marusiak@student.tuke.sk>
2025-03-07 11:46:10 +0100

    update startup user service

--- backend/user-service/app/main.py
+++ backend/user-service/app/main.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
-from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.database import Base, postgreDB
 from app.api.endpoints.user import router
-from sqlalchemy.future import select
 import py_eureka_client.eureka_client as eureka_client
+from app.config import EUREKA_CLIENT_SERVICEURL_DEFAULTZONE
+from contextlib import asynccontextmanager
 
-
-app = FastAPI()
-
-EUREKA_SERVER = "http://discovery-service:8761/eureka/"  # Адрес Eureka Server
+EUREKA_SERVER = EUREKA_CLIENT_SERVICEURL_DEFAULTZONE
 SERVICE_NAME = "user-service"
 SERVICE_PORT = 8000
 
@@ -16,20 +13,22 @@ async def init_eureka():
     await eureka_client.init_async(
         eureka_server=EUREKA_SERVER,
         app_name=SERVICE_NAME,
-        instance_port=SERVICE_PORT
+        instance_port=SERVICE_PORT,
+        renewal_interval_in_secs=30,
+        duration_in_secs=90,
     )
 
-# Ensure that the database tables are created asynchronously
 async def create_db():
     async with postgreDB.engine.begin() as conn:
-        # Create all tables (this works asynchronously)
         await conn.run_sync(Base.metadata.create_all)
 
-# Run this method when the application starts
-@app.on_event("startup")
-async def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     await create_db()
     await init_eureka()
+    yield
+    print("Shutting down the application...")
 
-# Include the user router
+app = FastAPI(lifespan=lifespan)
+
 app.include_router(router)
