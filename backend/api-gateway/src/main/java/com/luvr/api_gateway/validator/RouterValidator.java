79925e9fb41731246739275ce02005e55968b8d3
Maksym Norokha <maxnorohich@gmail.com>
2025-03-19 23:47:08 +0100

    added authentication check

---
+++ backend/api-gateway/src/main/java/com/luvr/api_gateway/validator/RouterValidator.java
@@ -0,0 +1,19 @@
+package com.luvr.api_gateway.validator;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Service
+public class RouterValidator {
+
+    private static final List<String> openEndpoints = List.of("/auth/login", "/auth/register");
+
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openEndpoints.stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+}
